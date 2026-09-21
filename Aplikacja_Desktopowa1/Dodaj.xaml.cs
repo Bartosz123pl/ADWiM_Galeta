db451a5193b2ff7bee0ae10fddc1828adec6f0fa
Bartek123pl <marine.daiwa@gmail.com>
2026-09-21 22:26:44 +0200

    Dodaj pliki projektów.

---
+++ Aplikacja_Desktopowa1/Dodaj.xaml.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Aplikacja_Desktopowa1
+{
+    /// <summary>
+    /// Logika interakcji dla klasy Dodaj.xaml
+    /// </summary>
+    public partial class Dodaj : Window
+    {
+        public Dodaj()
+        {
+            InitializeComponent();
+
+
+        }
+        private void Zapisz(object sender, RoutedEventArgs e)
+        {
+            char dziesiataCyfraChar = input4.Text[9];
+            int cyfraPlci = int.Parse(dziesiataCyfraChar.ToString());
+            List<Pracownik> listaPracownikow = new List<Pracownik>();
+            Pracownik nowyPracownik = new Pracownik();
+            nowyPracownik.Imie = input1.Text;
+            nowyPracownik.Nazwisko = input2.Text;
+            nowyPracownik.Wyplata = input3.Text;
+            nowyPracownik.M = (cyfraPlci % 2 != 0);
+            nowyPracownik.PESEL = input4.Text;
+            nowyPracownik.Menadzer = (bool)menadzer.IsChecked;
+            listaPracownikow.Add(nowyPracownik);
+
+        }
+    }
+}
---
+++ Aplikacja_Desktopowa1/Pracownik.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Aplikacja_Desktopowa1
+{
+    public class Pracownik
+    {
+        public string Imie { get; set; }
+        public string Nazwisko { get; set; }
+        public bool M { get; set; }
+        public string Wyplata { get; set; }
+        public string PESEL { get; set; }
+        public bool Menadzer { get; set; }
+    }
+}
